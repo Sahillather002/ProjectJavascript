c9de4125a92c68ea11556af98c1be7550170fedf
SahilGrip <sahillather002@gmail.com>
2024-02-14 20:24:56 +0530

    starting up

---
+++ pages/project1/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>Project 1</div>;
+};
+
+export default index;
---
+++ pages/project2/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>project 2</div>;
+};
+
+export default index;
